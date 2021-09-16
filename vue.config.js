b92d57b46bbd7ecc009650caada690f36e5e4a2d
adrian6922 <vargaszambranoj@gmail.com>
2021-09-16 19:25:50 -0300

    configuracion nueva

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production' ? '/deploy/' : '/',
+    // publicPath: process.env.NODE_ENV === 'production' ? '/deploy/' : '/',
+    publicPath: '/deploy/'
 }
